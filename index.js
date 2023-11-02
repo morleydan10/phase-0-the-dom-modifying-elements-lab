03c8daed0494e4df81317b8b47514ac409288f9d
Daniel Morley <morleydan10@gmail.com>
2023-11-02 08:17:54 -0400

    phase-1-the-dom-modifying-elements-solutions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+
+// let element = document.createElement('div')
+
+// document.body.append(div)
+
+// let ul = document.createElement('ul');
+
+// for (let i=0; i<3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i+1).toString();
+//     ul.append(li);
+// }
+
+let main = document.getElementById('main');
+main.remove();
+
+let h1 = document.createElement ('h1');
+document.body.appendChild(h1);
+
+let newHeader = document.createElement('H1')
+h1.appendChild(newHeader)
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Dan Morley is the champion"
+
